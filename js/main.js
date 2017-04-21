1f57982120485ba9e4427f9ca913bbeb69210273
Ana Durand <ana.dc1004@gmail.com>
2017-04-21 10:16:10 -0500

    agregando header

--- js/main.js
+++ js/main.js
@@ -7,13 +7,27 @@ window.addEventListener("load", function(){
   var header = crear("header", "header");
   var divRight = crear("div", "header-right");
   var spanLogo = crear("span", "logo");
-  
+  var img = crear("img", "foto");
+  var spanTw = crear("span", "tw");
+  var spanFc = crear("span", "face");
   var divLeft = crear("div", "header-left");
+  var spanLogo = crear("span", "logo");
+  var img = crear("img", "foto");
+  var spanSearch = crear("span","search");
+  var spanEdit = crear("span", "edit");
+
   var content = crear("section", "container");
   var footer = crear("footer", "footer");
 
 
   divRight.appendChild(spanLogo);
+  divRight.appendChild(img);
+  divRight.appendChild(spanTw);
+  divRight.appendChild(spanFc);
+  divLeft.appendChild(spanLogo);
+  divLeft.appendChild(img);
+  divLeft.appendChild(spanSearch);
+  divLeft.appendChild(spanEdit);
   header.appendChild(divRight);
   header.appendChild(divLeft);
   box.appendChild(header);
